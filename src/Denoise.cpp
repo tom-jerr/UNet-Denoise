95836ea1b86fd7b0479662e37081d64c10f194fa
lzy <2584074296@qq.com>
2023-11-30 23:45:29 +0800

    add Log

--- src/Denoise.cpp
+++ src/Denoise.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <stdio.h>
 // torch heade
 #include "torch/torch.h"
 #include "torch/script.h"
@@ -10,16 +10,21 @@
 #include <opencv2/opencv.hpp>
 
 #include "../include/CLDenoise.h"
+#include "../include/CLLog.h"
 
-
+// 全局变量
 extern std::string g_model_path;
 extern std::shared_ptr<torch::jit::script::Module> g_model;
 extern std::vector<cv::Mat> g_img_vec;
 extern std::vector<std::thread> g_thread_vec;
 std::string img_path = "../../pic/noise_Testing.png";
-// 加载模型
-void InitModel() {
+std::string log_path = "../../Log/DenoiseLog.txt";  
+//FILE* fp = fopen(log_path.c_str(), "a");
+Log* g_log;
+// 去噪初始化
+void Init() {
 	g_model = std::make_shared<torch::jit::script::Module>(torch::jit::load(g_model_path));
+    g_log = Log::getLogInstance(log_path);   
 }
 
 void MainLoopDenoise() {
@@ -42,9 +47,15 @@ void MainLoopDenoise() {
             std::cout << "g_img_vec size: " << g_img_vec.size() << std::endl;
 			DenoiseOP denoise_op;
             denoise_op.LoadImage(g_img_vec[0]);
+            time_t begin = time(0);
             std::thread denoise_thread(&DenoiseOP::DenoiseUML, &denoise_op);
             denoise_thread.join();
-
+            // 记录日志
+            time_t end = time(0);
+            std::cout << "Denoise time: " << end - begin << "s" << std::endl;
+            std::string log_msg = "Denoise time: " + std::to_string(end - begin) + "s";
+            g_log->writeLog(log_msg.c_str());
+            // 将去噪后的图片发送给前端
 			g_img_vec.erase(g_img_vec.begin());
 		}
         
@@ -57,10 +68,11 @@ int main() {
     //std::shared_ptr<torch::jit::script::Module> model = std::make_shared<torch::jit::script::Module>(model_path);
     std::cout << "model path: " << g_model_path << std::endl;
     std::cout << "img path: " << img_path << std::endl;
-
+    //fprintf(fp, "global model path: %s\n", g_model_path.c_str());
+    //fprintf(fp, "global img path: %s\n", img_path.c_str());
     std::cout << "init model" << std::endl;
-    InitModel();
-
+    Init();
+    g_log->writeLog("Denoise success");
     std::cout << "main loop begin" << std::endl;
     MainLoopDenoise();
     return 0;
